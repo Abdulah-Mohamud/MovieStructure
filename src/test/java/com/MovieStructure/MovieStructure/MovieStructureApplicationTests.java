5f6d8aad864bc2a0e6ea33044a8b7faba44a8e6c
abdul <abdul2213@gmail.comgit commitgit config --global user.email abdul2213@gmail.com>
2021-11-04 13:08:19 +0000

    first commit

--- src/test/java/com/MovieStructure/MovieStructure/MovieStructureApplicationTests.java
+++ src/test/java/com/MovieStructure/MovieStructure/MovieStructureApplicationTests.java
@@ -9,30 +9,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class MovieStructureApplicationTests {
 
-	Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
-
 	@Test
 	public void testForFilmTitle(){
+		Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
 		assertEquals("Film1",testFilm.getTitle(),"Film was not found");
 	}
 
 	@Test
 	public void testForDescription(){
+		Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
 		assertEquals("Film Description",testFilm.getDescription(),"Film Description was not found");
 	}
 
 	@Test
 	public void testForLength(){
+		Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
 		assertEquals(60,testFilm.getLength(),"Desired Film length was not found");
 	}
 
 	@Test
 	public void testForRating(){
+		Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
 		assertEquals("PG",testFilm.getRating(),"Desired rating was not found");
 	}
 
 	@Test
 	public void testForLanguage(){
+		Film testFilm = new Film(1,"Film1","Film Description",60,"PG",1);
 		assertEquals(1,testFilm.getLanguage_id(),"Film is in its desired language");
 	}
 
